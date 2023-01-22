388d07430310799a74a1438915e928b16ad9c9db
arpitg12 <121594026+arpitg12@users.noreply.github.com>
2023-01-22 14:21:47 +0530

    Add files via upload

--- cpp.cpp
+++ cpp.cpp
@@ -11,9 +11,12 @@ int enter;
 cout<<"Press 1 to enter Car"<<endl;
 cout<<"Press 2 to enter Bike"<<endl;
 cout<<"Press 3 to enter Cycle"<<endl;
-cout<<"Press 4 to show record"<<endl;
-cout<<"Press 5 to delete the record"<<endl;
-cout<<"Press 6 for exit"<<endl;
+cout<<"Press 4 to exit Car"<<endl;
+cout<<"Press 5 to exit Bike"<<endl;
+cout<<"Press 6 to exit Cycle"<<endl;
+cout<<"Press 7 to show record"<<endl;
+cout<<"Press 8 to delete the record"<<endl;
+cout<<"Press 9 for exit"<<endl;
 cin>>enter;
 
 if(enter==1){
@@ -22,30 +25,46 @@ if(enter==1){
     cout<<"Car is added successfully"<<endl;
 }
 else if(enter==2){
-   
-    Bikes++;
+     Bikes++;
      system("CLS");
     cout<<"Bikes is added successfully"<<endl;
 }
 else if(enter==3){
-   
-    Cycles++;
+   Cycles++;
      system("CLS");
     cout<<"Cycles is added successfully"<<endl;
 }
 else if(enter==4){
+   
+    Cars--;
+     system("CLS");
+    cout<<"Car is exited successfully"<<endl;
+}
+else if(enter==5){
+   
+    Bikes--;
+     system("CLS");
+    cout<<"Bike is exited successfully"<<endl;
+}
+else if(enter==6){
+   
+    Cycles--;
+     system("CLS");
+    cout<<"Cycle is exited successfully"<<endl;
+}
+else if(enter==7){
     cout<<"Cars=> "<<Cars<<endl;
     cout<<"Bikes=> "<<Bikes<<endl;
     cout<<"Cycles=> "<<Cycles<<endl;
 }
-else if(enter==5){
+else if(enter==8){
     Cars=0;
     Bikes=0;
     Cycles=0;
     system("CLS");
     cout<<"Record deleted successfully.....!!"<<endl;
 }
-else if(enter==6){
+else if(enter==9){
     exit(0);
 }
 else{
